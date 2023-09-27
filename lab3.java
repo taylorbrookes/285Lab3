34ce8ce92edfec62dc3daa99fd78be3ec012ffca
obeli <obeli@100.66.133.233>
2023-09-27 16:23:47 -0400

    Creating Lab3.java

---
+++ lab3.java
@@ -0,0 +1,18 @@
+/**
+ * lab3 - Instance of myXMLValidator is made. 
+ * Output of method precision is printed.
+ * @author Taylor Brookes
+ * @version 27 September 2023
+ */
+public class lab3 {
+	public static void main(String[] args) {
+		/**
+		 * this creates an instance of myXMLValidator
+		 */
+		IXMLValidator testCase = new myXMLValidator(INPUT);
+		/**
+		 * this outputs the precision of the method
+		 */
+		System.out.println(PRECISION);
+	}
+}
