ca0f1dea5c2b3262b7f61d3036a303904d7d74ee
Taylor Brookes <144271566+taylorbrookes@users.noreply.github.com>
2023-09-27 16:30:48 -0400

    Update IStack.java

--- IStack.java
+++ IStack.java
@@ -1,3 +1,8 @@
+/**
+ * IStack - Interface that instantiates methods
+ * @author Taylor Brookes
+ * @version 27 September 2023
+*/
 public interface IStack {
   void push (String c);
   String pop();
