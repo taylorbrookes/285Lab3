5a9a07d36a15a7099cb01db3c5d898e677da7500
Taylor Brookes <144271566+taylorbrookes@users.noreply.github.com>
2023-09-27 16:31:04 -0400

    Update IXMLValidator.java

--- IXMLValidator.java
+++ IXMLValidator.java
@@ -1,3 +1,8 @@
+/**
+ * IXMLValidator - Interface that instantiates methods
+ * @author Taylor Brookes
+ * @version 27 September 2023
+*/
 public interface IXMLValidator {
   boolean validateXML(IStack stack, String inputXML);
 }
